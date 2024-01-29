6548479c72df0e182334edbb6f04572255203159
iulian.radulescu <iulian.radulescu@onesoftware.ro>
2024-01-29 20:35:08 +0200

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', 'nuxt-vuefire', '@pinia/nuxt'],
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
   devtools: { enabled: true },
-  vuefire: {
-    config: {
-      apiKey: 'AIzaSyB_xo7O057IALRa_75xukOUYuJ7RAtldgo',
-      authDomain: 'palworld-breeding-de4fc.firebaseapp.com',
-      projectId: 'palworld-breeding-de4fc',
-      storageBucket: 'palworld-breeding-de4fc.appspot.com',
-      messagingSenderId: '830867244966',
-      appId: '1:830867244966:web:27854c7dd4a429ba8d7167',
-      // there could be other properties depending on the project
-    },
-  },
 })
