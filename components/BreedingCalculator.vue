da6d6a743747d58eff57f843e7ed13a9da07365d
iulian.radulescu <iulian.radulescu@onesoftware.ro>
2024-02-03 18:59:27 +0200

    Added unique breeding discovery and breeding matrix

--- components/BreedingCalculator.vue
+++ components/BreedingCalculator.vue
@@ -1,83 +1,117 @@
 <template>
-<UContainer  class="dark:bg-gray-900 max-w-fit max-h-fit py-2 items-center justify-center"  :ui="{ base: '', ring: '', divide: 'divide-y divide-gray-200', header: { padding: 'px-4 py-5' }, body: { padding: '', base: 'divide-y divide-gray-200 dark:divide-gray-700' }, footer: { padding: 'p-4' } }">
-    <UForm  :validate="validate" :state="state" class="space-y-4 items-center justify-center" @submit="onSubmit" @error="onError">
-        <h1 class="text-center text-3xl font-bold">CHILD CALCULATOR</h1>
-        <div class="flex space-x-4">
+  <div class="dark:bg-gray-900 py-2 flex px-5">
+    <div class="flex flex-col hide-scrollbar mr-4">
+      <div class="max-h-[350px] overflow-y-auto flex flex-col  hide-scrollbar items-center ">
+        <UForm :validate="validateCalculator" :state="stateCalculator" class="space-y-4 items-center justify-center" @submit="onSubmitCalculator" @error="onError">
+          <h1 class="text-center text-3xl font-bold py-3">Parent Breeding Discovery</h1>
+          <div class="flex space-x-4">
             <UFormGroup label="Parent 1" name="parent1">
-                <USelectMenu 
-                    v-model="state.parent1" 
-                    placeholder="Select..." 
-                    :options="selectOptions" 
-                    clear-search-on-close 
-                    searchable 
-                    searchable-placeholder="Search a pal..."
-                    class="me-2 w-36"
-                    />
+              <USelectMenu v-model="stateCalculator.parent1" placeholder="Select..." :options="selectOptions" clear-search-on-close searchable searchable-placeholder="Search a pal..." class="me-2 w-36" />
             </UFormGroup>
-
             <UFormGroup label="Parent 2" name="parent2">
-                <USelectMenu v-model="state.parent2" 
-                    placeholder="Select..." 
-                    :options="selectOptions" 
-                    clear-search-on-close 
-                    searchable 
-                    searchable-placeholder="Search a pal..."
-                    :ui="{ width: 'w-auto' }"
-                    class="me-2 w-36"
-                    />
+              <USelectMenu v-model="stateCalculator.parent2" placeholder="Select..." :options="selectOptions" clear-search-on-close searchable searchable-placeholder="Search a pal..." :ui="{ width: 'w-auto' }" class="me-2 w-36" />
             </UFormGroup>
-            <UButton type="submit" icon='i-heroicons-magnifying-glass-circle' variant="link" size="xl">
-              Breed
-            </UButton>
-        </div>
-  </UForm>
-  <UTable class="items-center justify-center pt-3 w-80" 
-  :rows="rows"
-  :columns="columns"
-  :loading="loading"
-  :emptyState="{icon: '', label: ''}"
-  :ui="{'th': {'base': 'text-center'}, 'td': {'base': 'whitespace-nowrap text-center'}}"
-   />
-</UContainer>
+            <UButton type="submit" icon='i-heroicons-magnifying-glass-circle' variant="link" size="xl">Breed</UButton>
+          </div>
+        </UForm>
+        <UTable class="items-center justify-center pt-3 w-full" :rows="rowsCalculator" :columns="columnsCalculator" :loading="loadingCalculator" :emptyState="{icon: '', label: ''}" :ui="{'th': {'base': 'text-center'}, 'td': {'base': 'whitespace-nowrap text-center'}}" />
+      </div>
+      <div class="max-h-[600px] overflow-y-auto hide-scrollbar">
+        <h1 class="text-center text-3xl font-bold py-3">Parent Unique Breeding Discovery</h1>
+        <UTable class="items-center justify-center" 
+          :rows="rowUnuqies"
+          :columns="columnsUnique"
+          :emptyState="{icon: '', label: ''}"
+        />
+      </div>
+    </div>
+    <UDivider orientation="vertical" class="w-1 pl-2 pr-6"  />
+    <div class="max-h-[950px] overflow-y-auto hide-scrollbar flex flex-col  w-4/5 items-center">
+      <UForm :validate="validateMatrix" :state="stateMatrix" class="space-y-4 items-center justify-center" @submit="onSubmitMatrix" @error="onError">
+          <h1 class="text-center text-3xl font-bold py-3">Pal Breeding Matrix</h1>
+          <div class="flex space-x-4">
+            <UFormGroup label="Parent" name="parent">
+              <USelectMenu v-model="stateMatrix.parent" placeholder="Select..." :options="selectOptions" clear-search-on-close searchable searchable-placeholder="Search a pal..." class="me-2 w-36" />
+            </UFormGroup>
+            <UButton type="submit" icon='i-heroicons-magnifying-glass-circle' variant="link" size="xl">Show matrix</UButton>
+          </div>
+        </UForm>
+        <UTable class="items-center justify-center pt-3 w-full  hide-scrollbar" :rows="rowsMatrix" :columns="columnsMatrix" :loading="loadingMatrix" :emptyState="{icon: '', label: ''}" :ui="{'th': {'base': 'text-center'}, 'td': {'base': 'whitespace-nowrap text-center'}}" />
+    </div>
+  </div>
 </template>
+
 
 
 <script setup lang="ts">
 import type { FormError, FormErrorEvent, FormSubmitEvent } from '#ui/types'
 import { palStore } from '~/store/palStore';
-import { findChild } from '~/services/mongodb';
+import { findChild, getUniques, getMatrix } from '~/services/mongodb';
 import type { PalBreeding } from '~/types/PalBreeding'
 
-const rows = ref<{child: String}[]>([])
-const loading = ref<Boolean>(false)
+const rowsCalculator = ref<{child: String}[]>([])
+const rowsMatrix = ref<PalBreeding[]>([])
+const loadingCalculator = ref<Boolean>(false)
+const loadingMatrix = ref<Boolean>(false)
 const store = palStore()
-const state = reactive({
+const stateCalculator = reactive({
   parent1: undefined,
-  parent2: undefined
+  parent2: undefined,
 })
-const columns = [
+
+const stateMatrix = reactive({
+  parent: undefined
+})
+
+const columnsCalculator = [
     {key: 'child', label: "Child"}
 ]
+
+const columnsUnique = [
+    {key: 'child', label: "Child", sortable: true, class: "String"},
+    {key: 'parent1', label: "Parent 1", sortable: true, class: "String"},
+    {key: 'parent2', label: "Parent 2", sortable: true, class: "String"},
+]
+
+const columnsMatrix = [
+    {key: 'child', label: "Child", sortable: true, class: "String"},
+    {key: 'parent1', label: "Parent 1", sortable: true, class: "String"},
+    {key: 'parent2', label: "Parent 2", sortable: true, class: "String"},
+]
+
 const selectOptions = store.rows.map((row) => ({
   label: row.name,
   value: row.name, // You can use a different property based on what you want to set as the selected value
 }));
 
 
-const validate = (state: any): FormError[] => {
+const validateCalculator = (stateCalculator: any): FormError[] => {
   const errors = []
-  if (!state.parent1) errors.push({ path: 'parent1', message: 'Required' })
-  if (!state.parent2) errors.push({ path: 'parent2', message: 'Required' })
+  if (!stateCalculator.parent1) errors.push({ path: 'parent1', message: 'Required' })
+  if (!stateCalculator.parent2) errors.push({ path: 'parent2', message: 'Required' })
   return errors
 }
 
-async function onSubmit (event: FormSubmitEvent<any>) {
-    loading.value = true;
+const validateMatrix = (stateCalculator: any): FormError[] => {
+  const errors = []
+  if (!stateCalculator.parent) errors.push({ path: 'parent', message: 'Required' })
+  return errors
+}
+
+async function onSubmitCalculator (event: FormSubmitEvent<any>) {
+  loadingCalculator.value = true;
     const response :PalBreeding[] = await findChild('palworld_db', 'pal_breeding', event.data)
-    rows.value = response.map((row) => ({
+    rowsCalculator.value = response.map((row) => ({
         child: row.child
     }));
-    loading.value = false;
+    loadingCalculator.value = false;
+}
+
+async function onSubmitMatrix (event: FormSubmitEvent<any>) {
+  loadingMatrix.value = true;
+  const reponse: PalBreeding[] = await getMatrix('palworld_db', 'pal_breeding', event.data)
+  rowsMatrix.value = reponse;
+  loadingMatrix.value = false;
 }
 
 async function onError (event: FormErrorEvent) {
@@ -85,4 +119,23 @@ async function onError (event: FormErrorEvent) {
   element?.focus()
   element?.scrollIntoView({ behavior: 'smooth', block: 'center' })
 }
+
+const parent1Options = ['Relaxaurus', 'Incineram', 'Mau', 'Vanwyrm', 'Eikthyrdeer', 'Elphidran', 'Pyrin', 'Mammorest', 'Mossanda', 'Dinossom', 'Jolthog', 'Frostallion', 'Kingpaca', 'Lyleen', 'Leezpunk', 'Blazehowl', 'Robinquill', 'Broncherry', 'Surfent', 'Gobfin', 'Suzaku', 'Reptyro', 'Hangyu', 'Mossanda', 'Vanwyrm', 'Mossanda', 'Grizzbolt', 'Kitsun'];
+const parent2Options = ['Sparkit', 'Maraith', 'Pengullet', 'Foxcicle', 'Hangyu', 'Surfent', 'Katress', 'Wumpo', 'Grizzbolt', 'Rayhound', 'Pengullet', 'Helzephyr', 'Reindrix', 'Menasting', 'Flambelle', 'Felbat', 'Fuddler', 'Fuack', 'Dumud', 'Rooby', 'Jormuntide', 'Foxcicle', 'Swee', 'Petallia', 'Anubis', 'Rayhound', 'Relaxaurus', 'Astegon',];
+
+const parentSets: { parent1: { value: string }; parent2: { value: string } }[] = [];
+
+for (let i = 0; i < parent1Options.length && i < parent2Options.length; i++) {
+  const combination1 = {
+    parent1: { value: parent1Options[i] },
+    parent2: { value: parent2Options[i] }
+  };
+  const combination2 = {
+    parent1: { value: parent2Options[i] },
+    parent2: { value: parent1Options[i] }
+  };
+  parentSets.push(combination1);
+  parentSets.push(combination2);
+}
+const rowUnuqies :PalBreeding[] = await getUniques('palworld_db', 'pal_breeding', parentSets);
 </script>
--- services/mongodb.ts
+++ services/mongodb.ts
@@ -1,6 +1,4 @@
-import { ref, computed, onMounted } from 'vue';
 import * as Realm from 'realm-web';
-import type { PalType } from '~/types/PalType';
 
 export const findCollection = async <T>(dbName: string, collectionName: string): Promise<T[]> => {
     const app = new Realm.App({ id: 'data-itvjz' });
@@ -24,5 +22,35 @@ export const findChild = async <T>(dbName: string, collectionName: string, paren
         const results: T[] = await collection.find({'parent1': parents['parent2']['value'], 'parent2': parents['parent1']['value']});
         return results;
     }
+    return results;
+}
+
+export const getUniques = async <T>(dbName: string, collectionName: string, parentSets: any[]): Promise<T[]> => {
+    const app = new Realm.App({ id: 'data-itvjz' });
+    const credentials = Realm.Credentials.apiKey('fWgqvs2VeZMk0NQ5OFtg2nP5I3GBEoZi5ZrAcbLkPkchtmbASrYvkNkxivS2Banb');
+    await app.logIn(credentials);
+    const mongo = app.currentUser.mongoClient('Palworld');
+    const collection = mongo.db(dbName).collection(collectionName);
+
+    const orClauses = parentSets.map(parentSet => ({
+        'parent1': parentSet['parent1']['value'],
+        'parent2': parentSet['parent2']['value']
+    }));
+
+    const results: T[] = await collection.find({
+        $or: orClauses
+    });
+
+    return results;
+}
+
+export const getMatrix = async <T>(dbName: string, collectionName: string, parent: any[]): Promise<T[]> => {
+    const app = new Realm.App({ id: 'data-itvjz' });
+    const credentials = Realm.Credentials.apiKey('fWgqvs2VeZMk0NQ5OFtg2nP5I3GBEoZi5ZrAcbLkPkchtmbASrYvkNkxivS2Banb');
+    await app.logIn(credentials);
+    const mongo = app.currentUser.mongoClient('Palworld');
+    const collection = mongo.db(dbName).collection(collectionName);
+
+    const results: T[] = await collection.find({ $or: [{ parent1: parent['parent']['value'] }, { parent2: parent['parent']['value'] }] });
     return results;
 }
